6375d80a1c4018a4684eae8d61c92b15c0eb5b04
Arthur <arthurboutry2005@icloud.com>
2024-11-28 14:16:59 +0100

    Ajout fichier

---
+++ exercice.py
@@ -0,0 +1,30 @@
+
+def lister_todos():
+    pass
+
+def creer_todo():
+    pass
+
+def modifier_statut_todo():
+    pass
+    
+def supprimer_todo():
+    pass
+
+choix = ''
+while choix != 'q':
+    print('\n==== Menu principal ====')
+    print('1: Lister les todos')
+    print('2: Créer un todo')
+    print('3: Changer le statut d\'un todo')
+    print('4: Supprimer un todo')
+    print('q: quitter')
+    print('========================')
+    choix = input('=> Choix : ')
+    match choix:
+        case '1': lister_todos()
+        case '2': creer_todo()
+        case '3': modifier_statut_todo()
+        case '4': supprimer_todo()
+        case 'q': print('Au revoir')
+        case _: print('Choix invalide')
